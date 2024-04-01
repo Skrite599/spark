5db3cda372105c3f59dfeb3d02f8063b0a2a1643
Skrite599 <sprite599@gmail.com>
2024-04-01 13:26:59 -0400

    [Testing] Deck-Profile

--- views.py
+++ views.py
@@ -26,7 +26,7 @@ def deck_profile(deck_id):
   deck = get_deck(deck_id)
   record = get_record(request.headers)
 
-  deck['wins'] = record['win'] if record['win'] else None
-  deck['loss'] = record['loss'] if record['loss'] else None
+  # deck['wins'] = record['win'] if record['win'] else None
+  # deck['loss'] = record['loss'] if record['loss'] else None
 
-  return render_template('deck-profile.html')
+  return render_template('deck-profile.html', deck=deck, record=record)
