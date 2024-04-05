326e7e6bb500b880b6a7bcd52517d01ff05ecbcc
Skrite599 <sprite599@gmail.com>
2024-04-05 20:08:14 +0000

    updated app pw

--- main.py
+++ main.py
@@ -22,14 +22,15 @@ def server_login():
     session['username'] = data.get('username')
     session['user_id'] = response['user_id']
     return jsonify(response), 200
-  
+
+
 @app.route('/api/logout', methods=['POST'])
 def server_logout():
   if request and 'user_id' in session:
     del session['user_id']
     response = {
-      'status': 'success',
-      'message': 'user was successfully logged out'
+        'status': 'success',
+        'message': 'user was successfully logged out'
     }
     return jsonify(response), 200
 
@@ -50,17 +51,19 @@ def server_submit_game():
     user_id = session['user_id']
     response = submit_game(data, user_id)
     if (response['status'] == 'error'):
-      return jsonify(response), 400 
+      return jsonify(response), 400
     return jsonify(response), 200
-  
+
+
 @app.route('/api/users/', methods=['GET'])
 def server_get_users():
   if request and request.args.get('search'):
     username_query = request.args.get('search')
     response = query_user(username_query)
     if (response['status'] == 'error'):
-      return jsonify(response), 400 
+      return jsonify(response), 400
     return jsonify(response), 200
+
 
 @app.route('/api/deck', methods=['GET'])
 def server_get_deck():
@@ -69,7 +72,8 @@ def server_get_deck():
     response = get_decks(session_id)
     return jsonify(response), 200
 
-@app.route('/send', methods=['GET'])  
+
+@app.route('/send', methods=['GET'])
 def send_email():
   contents = 'hello Martin, this is a test'
 
@@ -80,8 +84,9 @@ def send_email():
   msg['To'] = ', '.join('sprite599@gmail.com')
 
   with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-       smtp_server.login('martin.liriano@gmail.com', 'hammerd009')
-       smtp_server.sendmail('martin.liriano@gmail.com', 'sprite599@gmail.com', msg.as_string())
+    smtp_server.login('martin.liriano@gmail.com', 'ccez ekei usjr egmv')
+    smtp_server.sendmail('martin.liriano@gmail.com', 'sprite599@gmail.com',
+                         msg.as_string())
 
 
 if __name__ == '__main__':
