57667e276d7cd144515d4fb7145cacacb3747c14
Skrite599 <sprite599@gmail.com>
2024-04-12 20:03:54 -0400

    refactor css/html for responsiveness

--- static/js/script.js
+++ static/js/script.js
@@ -84,14 +84,15 @@ async function populateUserResults(query) {
 function submitDeck(form_data) {
 
     var request_body = {}
+    const activeElement = form_data.querySelector('[status="active"]');
 
-    if (form_data['deck_name'] && form_data['deck_score']) {
+    if (form_data['deck_name'] && activeElement) {
         request_body = {
             'deck_name' : form_data['deck_name'].value,
-            'deck_score' : form_data['deck_score'].value
+            'deck_score' : activeElement.value
         }
     }
-
+    
     var request_body = JSON.stringify(request_body);
 
     fetch('/api/deck', {
@@ -109,6 +110,49 @@ function submitDeck(form_data) {
     });
 
     return false;
+}
+
+function setToolTip(score) {
+
+    const form_data = document.getElementById('deck')
+
+    const tooltipContainer = document.querySelector('.form .tooltip.container');
+
+    score = Number(score)
+
+    const tooltipLow = form_data.querySelector('.tooltip.container .low');
+    const tooltipMed = form_data.querySelector('.tooltip.container .med');
+    const tooltipHigh = form_data.querySelector('.tooltip.container .high');
+    const tooltipCEDH = form_data.querySelector('.tooltip.container .cedh');
+
+    if (score >= 1 && score <= 5) {
+        tooltipLow.style.display = 'block';
+        tooltipMed.style.display = 'none';
+        tooltipHigh.style.display = 'none';
+        tooltipCEDH.style.display = 'none';
+    } else if (score == 6) {
+        tooltipLow.style.display = 'none';
+        tooltipMed.style.display = 'block';
+        tooltipHigh.style.display = 'none';
+        tooltipCEDH.style.display = 'none';
+    } else if (score >= 7 && score <= 9) {
+        tooltipLow.style.display = 'none';
+        tooltipMed.style.display = 'none';
+        tooltipHigh.style.display = 'block';
+        tooltipCEDH.style.display = 'none';
+    } else if (score == 10) {
+        tooltipLow.style.display = 'none';
+        tooltipMed.style.display = 'none';
+        tooltipHigh.style.display = 'none';
+        tooltipCEDH.style.display = 'block';
+    } else {
+        tooltipLow.style.display = 'none';
+        tooltipMed.style.display = 'none';
+        tooltipHigh.style.display = 'none';
+        tooltipCEDH.style.display = 'none';
+    }
+
+    tooltipContainer.classList.add('active');
 }
 
 function submitGame(form_data) {
@@ -224,4 +268,100 @@ function signup(form_data) {
     });
 
     return false;
+}
+
+function shiftScore(button) {
+
+    const scoreValueDiv = document.querySelector('.score.container #value span');
+
+    const lowButton = document.querySelector(".score.container #level-selector button[value='low']");
+    const medButton = document.querySelector(".score.container #level-selector button[value='med']");
+    const highButton = document.querySelector(".score.container #level-selector button[value='high']");
+    const cedhButton = document.querySelector(".score.container #level-selector button[value='cedh']");
+
+    var scoreValue = scoreValueDiv.textContent;
+    scoreValue = Number(scoreValue);
+
+    if (button.classList.contains('prev') && scoreValue != 1) {
+        scoreValue -= 1;
+    } else if (button.classList.contains('next') && scoreValue != 10) {
+        scoreValue += 1;
+    }
+
+    scoreValueDiv.innerText = scoreValue;
+
+    if (scoreValue >= 1 && scoreValue <= 5) {
+        shiftButton(lowButton);
+    } else if (scoreValue == 6) {
+        shiftButton(medButton);
+    } else if (scoreValue >= 7 && scoreValue <= 9) {
+        shiftButton(highButton);
+    } else if (scoreValue == 10) {
+        shiftButton(cedhButton);
+    }
+
+    setToolTip(scoreValue);
+
+}
+
+function shiftButton(button) {
+
+    const levelButtons = document.querySelectorAll('button.selector');
+
+    levelButtons.forEach((levelButton) => {
+        var classList_active = levelButton.classList;
+        classList_active.remove('active');
+    });
+
+    button.classList.add('active');
+}
+
+function setScore(span) {
+
+    if (!span) return;
+
+    var score = 0;
+    var level = span.innerText;
+    const scoreValueDiv = document.querySelector('.score.container #value span');
+    const arrowImgs = document.querySelectorAll('.score.container .direction img');
+
+    if (level == 'Low') {
+        score = 5;
+    } else if (level == 'Medium') {
+        score = 6;
+    } else if (level == 'High') {
+        score = 7;
+    } else if (level == 'CEDH') {
+        score = 10;
+    }
+
+    scoreValueDiv.innerText = score;
+    arrowImgs.forEach((arrowImg) => {
+        arrowImg.classList.add('active');
+    });
+
+    setToolTip(score);
+}
+
+function toggleButton(button) {
+
+    const levelButtons = document.querySelectorAll('button.selector');
+
+    levelButtons.forEach((levelButton) => {
+        var classList_active = levelButton.classList;
+        classList_active.remove('active');
+    });
+
+    button.classList.add('active');
+
+    setScore(button.querySelector('span'));
+}
+
+function displayMenu(navA) {
+    var ultilityContainer = document.getElementById('link-container');
+    if (ultilityContainer.style.display === "block") {
+        ultilityContainer.style.display = "none";
+    } else {
+        ultilityContainer.style.display = "block";
+    }
 }
